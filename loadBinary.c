9398b5314e8c888e0ab8a1d37b0f2de9953b4f49
Binghao <wangbhsoon@gmail.com>
2015-10-15 16:22:04 -0400

    second

--- loadBinary.c
+++ loadBinary.c
@@ -6,19 +6,18 @@
 int main(){
     FILE *f;
     int i;
-    float k;
+    unsigned char buffer[100];
     
-    f = fopen(PATH, "r+");
+    f = fopen(PATH, "rb");
     if (f == NULL){
-        printf("Error");
+        printf("Unable to open file!");
         exit(1);
     }
 
     i = 0;
-    while (!feof(f)){
-        fread(&k, sizeof(double), 1, f);
-        printf("%f", k);
-        i++;
+    fread(buffer, sizeof(buffer), 1, f);
+    for (int i = 0; i < 100; i++){
+        printf("#%d is: %x\n", i, buffer[i]);
     }
 
     fclose(f);
