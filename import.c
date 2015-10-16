9d97fc52f097dbad4d8384a2089917d0f218da4e
devai <wangbhsoon@gmail.com>
2015-10-16 12:44:37 -0400

    comment

--- import.c
+++ import.c
@@ -4,23 +4,29 @@
 
 int main(void)
 {
-    const int N = 16; /* nb. neurons */
+    // Network structure
+    const int N = 16; 
     const int M = 9;
 
-    float *w = malloc(N*M*sizeof(*w)); /* weights */
-    float *b = malloc(N*sizeof(*b));   /* biases */
+    // Weights
+    float *w = malloc(N*M*sizeof(*w)); 
+    // Biases
+    float *b = malloc(N*sizeof(*b));   
 
+    // Binary file operations
     FILE *f = fopen("/Users/wbh/cnn/module1.bin", "rb");
     assert(fread(w, sizeof(*w), N*M, f) == N*M);
     assert(fread(b, sizeof(*b), N, f) == N);
     fclose(f);
 
+    // Output the weights
     int i, j;
     for (i = 0; i < N; i++)
         for (j = 0; j < M; j++){
             printf("w[%d,%d] = %f\n", i, j, w[M * i + j]);
         }
 
+    // Output the biases
     for (i = 0; i < N; i++){
         printf("b[%d] = %f\n", i, b[i]);
     }
