57bba2733ac3e7200f6019f0b9c17626ce4f5c46
devai <wangbhsoon@gmail.com>
2015-10-16 11:42:56 -0400

    import

--- import.c
+++ import.c
@@ -2,30 +2,31 @@
 #include <stdlib.h>
 #include <assert.h>
 
-int
-main(void)
+int main(void)
 {
-  const int N = 16; /* nb. neurons */
+    const int N = 16; /* nb. neurons */
     const int M = 9;
 
-  float *w = malloc(N*M*sizeof(*w)); /* weights */
-  float *b = malloc(M*sizeof(*b));   /* biases */
+    float *w = malloc(N*M*sizeof(*w)); /* weights */
+    float *b = malloc(N*sizeof(*b));   /* biases */
 
-  FILE *f = fopen("/Users/wbh/test.bin", "rb");
-  assert(fread(w, sizeof(*w), N*M, f) == N*M);
-  assert(fread(b, sizeof(*b), M, f) == M);
-  fclose(f);
+    FILE *f = fopen("/Users/wbh/cnn/module1.bin", "rb");
+    assert(fread(w, sizeof(*w), N*M, f) == N*M);
+    assert(fread(b, sizeof(*b), N, f) == N);
+    fclose(f);
 
-  int i, j;
-  for (i = 0; i < N; i++)
-    for (j = 0; j < M; j++)
-      printf("w[%d,%d] = %f\n", i, j, w[N*i+j]);
+    int i, j;
+    for (i = 0; i < N; i++)
+        for (j = 0; j < M; j++){
+            printf("w[%d,%d] = %f\n", i, j, w[M * i + j]);
+        }
 
-  for (i = 0; i < M; i++)
-      printf("b[%d] = %f\n", i, b[i]);
+    for (i = 0; i < N; i++){
+        printf("b[%d] = %f\n", i, b[i]);
+    }
 
-  free(w);
-  free(b);
+    free(w);
+    free(b);
 
-  return 0;
+    return 0;
 }
