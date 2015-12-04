6df70e613099d8fb57787678d78f7795c2452c00
devaib <wangbhsoon@gmail.com>
2015-12-04 16:28:15 -0500

    new 12 weightt

--- 12net/testdetect/build/CMakeFiles/detect.dir/cmake_clean.cmake
+++ 12net/testdetect/build/CMakeFiles/detect.dir/cmake_clean.cmake
@@ -1,10 +1,10 @@
-file(REMOVE_RECURSE
+FILE(REMOVE_RECURSE
   "CMakeFiles/detect.dir/detect.c.o"
   "detect.pdb"
   "detect"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/detect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
